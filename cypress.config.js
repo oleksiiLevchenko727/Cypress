9e0ff2a8d3c2725fd0300010abdc74fd5801b8c5
B1E6y <stillstillsb@gmail.com>
2025-12-09 00:12:44 +0200

    The tests with general describe block is completed

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://lms.ithillel.ua/",
+    baseUrl: "https://qauto.forstudy.space",
     setupNodeEvents(on, config) {
       
     },
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,13 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('loginByApi', () => {
+  cy.request({
+    method: 'POST',
+    url: '/api/login', 
+    body: {
+      username: Cypress.env('guest'),
+      password: Cypress.env('welcome2qauto')
+    }
+  }).then((resp) => {
+    window.localStorage.setItem('authToken', resp.body.token); 
+    cy.setCookie('session_id', resp.body.session_id); 
+  });
+});
